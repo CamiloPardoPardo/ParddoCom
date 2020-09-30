b453a3a0eb83397c9b731bfb9669a2dbf3e68458
Camilo Pardo <info@parddo.com>
2020-09-30 16:46:37 -0500

    Se dejo en total funcionmiento el area de la plataforma

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Carbon\Carbon;
 use App\Models\Registro;
 use Illuminate\Http\Request;
 
@@ -26,16 +27,35 @@ class HomeController extends Controller
 
     public function index()
     {
+
+
+
+        // Sistema para calculos generales de la plataforma 
         $BasePlataforma = 250000;
         $RecargasPlataforma =  Registro::all()->where('id_Area','3');
         $MoverRecargas = Registro::all()->where('id_Area','4');
         $VentaRecargas = Registro::all()->where('id_Area','1');
         $VentaSuperPagos = Registro::all()->where('id_Area','2');
 
+        //sistema para calculo de ventas por día
+
+
         function Sumar ($Num_1){
             $Resultado = 0;
             foreach ($Num_1 as $Filas) {                
                 $Resultado +=  $Filas->Valor;   
+            }
+            return($Resultado);
+        }
+
+        function SumarPorDia ($Filas){
+            $hoy = Carbon::now()->format('d-m-Y');
+            $Resultado = 0;
+            foreach ($Filas as $Fila) {
+                $FechaDeFila = $Fila->created_at->format('d-m-Y');
+                if($FechaDeFila == $hoy){
+                    $Resultado +=  $Fila->Valor;   
+                }                
             }
             return($Resultado);
         }
@@ -46,8 +66,19 @@ class HomeController extends Controller
         $EfectivoPlataforma = (Sumar($VentaSuperPagos)+ Sumar($VentaRecargas) + $BasePlataforma)- Sumar($RecargasPlataforma);
         $GananciaPlataforma = ($Superpagos + $Recargas + $EfectivoPlataforma)-$BasePlataforma;
 
+        $totalPlatforma = $Superpagos + $Recargas + $EfectivoPlataforma + $GananciaPlataforma;
 
-        return view('home', compact('Superpagos','Recargas','EfectivoPlataforma','GananciaPlataforma'));
+        $PorSuperpagos = ($Superpagos/$totalPlatforma) * 100;
+        $PorRecargas = ($Recargas/$totalPlatforma) * 100;
+        $PorEfectivoPlataforma = ($EfectivoPlataforma/$totalPlatforma) * 100;
+        $PorGananciaPlataforma = ($GananciaPlataforma/$totalPlatforma) * 100;
+
+        $VentaRecargashoy = SumarPorDia($VentaRecargas);
+        $VentaSuperPagoshoy = SumarPorDia($VentaSuperPagos);
+        $VentasPlataformaHoy = $VentaRecargashoy + $VentaSuperPagoshoy;
+
+
+        return view('home', compact('Superpagos','Recargas','EfectivoPlataforma','GananciaPlataforma', 'PorSuperpagos','PorRecargas','PorEfectivoPlataforma','PorGananciaPlataforma','VentasPlataformaHoy'));
     }
 
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
+
 <section class="content">
+<div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>$ {{ number_format($VentasPlataformaHoy, 0 ) }}</h3>
+
+                <p>Ventas plataforma</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>$ 53.000<sup style="font-size: 20px"></sup></h3>
+
+                <p>Ventas Negocio</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>$ 14.000</h3>
+
+                <p>Mano de obra</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>$ 6.500</h3>
+
+                <p>Gastos</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-6">
@@ -15,9 +79,9 @@
                   <thead>                  
                     <tr>
                       <th style="width: 10px">#</th>
-                      <th>Task</th>
-                      <th>Progress</th>
-                      <th style="width: 40px">Label</th>
+                      <th>Area</th>
+                      <th>Valor</th>
+                      <th style="width: 40px">%</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -96,9 +160,9 @@
                   <thead>                  
                     <tr>
                       <th style="width: 10px">#</th>
-                      <th>Task</th>
-                      <th>Progress</th>
-                      <th style="width: 40px">Label</th>
+                      <th>Area</th>
+                      <th>Valor</th>
+                      <th style="width: 40px">%</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -106,33 +170,33 @@
                       <td>1.</td>
                       <td>Superpagos</td>
                       <td>
-                          <h4><b>$ </b>{{ number_format($Superpagos, 2 ) }}</h4>
+                          <h4><b>$ </b>{{ number_format($Superpagos, 0 ) }}</h4>
                       </td>
-                      <td><span class="badge bg-primary">30%</span></td>
+                      <td><span class="badge bg-primary">{{ number_format($PorSuperpagos, 0 ) }} %</span></td>
                     </tr>
                     <tr>
                       <td>2.</td>
                       <td>Recargas</td>
                       <td>
-                          <h4><b>$ </b>{{ number_format($Recargas, 2 ) }}</h4>
+                          <h4><b>$ </b>{{ number_format($Recargas, 0 ) }}</h4>
                       </td>
-                      <td><span class="badge bg-warning">70%</span></td>
+                      <td><span class="badge bg-primary">{{ number_format($PorRecargas, 0 ) }} %</span></td>
                     </tr>
                     <tr>
                       <td>3.</td>
                       <td>Efectivo</td>
                       <td>
-                          <h4><b>$ </b>{{ number_format($EfectivoPlataforma, 2 ) }}</h4>
+                          <h4><b>$ </b>{{ number_format($EfectivoPlataforma, 0 ) }}</h4>
                       </td>
-                      <td><span class="badge bg-primary">30%</span></td>
+                      <td><span class="badge bg-primary">{{ number_format($PorEfectivoPlataforma, 0 ) }} %</span></td>
                     </tr>
                     <tr>
                       <td>4.</td>
                       <td>Ganancias</td>
                       <td>
-                          <h4><b>$ </b>{{ number_format($GananciaPlataforma, 2 ) }}</h4>
+                          <h4><b>$ </b>{{ number_format($GananciaPlataforma, 0 ) }}</h4>
                       </td>
-                      <td><span class="badge bg-success">90%</span></td>
+                      <td><span class="badge bg-primary">{{ number_format($PorGananciaPlataforma, 0 ) }} %</span></td>              
                     </tr>
                   </tbody>
                 </table>
